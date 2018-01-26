522774eaed59ab6a841a1c9eec68fc5618b17e78
Susan <suzutoyuki@gmail.com>
2018-01-26 22:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ return array;
 }
 
 function johnLennonFacts(array){
-  const facts = [
+  var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
