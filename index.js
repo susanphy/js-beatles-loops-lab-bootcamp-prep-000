388d70c5c6059c2afa8daa591a48225df833544b
Susan <suzutoyuki@gmail.com>
2018-01-26 23:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function johnLennonFacts(facts) {
 }
 return results;
 }
+
+function iLoveTheBeatles(){
+var array=[]
+do{
+  array.push('I love the Beatles!');
+  num++
+}while (num<15);
+return array;
+
+}
