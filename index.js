9a3bee42cc0049aa6cad07e1361441b6fcce449a
Susan <suzutoyuki@gmail.com>
2018-01-26 22:09:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function johnLennonFacts(array){
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
-var count = 0
+var count = 0;
 while (count<array.length){
   console.log("`$[facts]`+ !!!");
 
