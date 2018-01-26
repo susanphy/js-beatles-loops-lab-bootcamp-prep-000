bc74aeb68b825b87c7392cab991f0e87c9331d8d
Susan <suzutoyuki@gmail.com>
2018-01-26 21:45:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-+function theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
 var array = [];
  for (var i = 0; i < musicians.length; i++) {
  array.push(musicians[i] + ' plays ' + instruments[i]);
